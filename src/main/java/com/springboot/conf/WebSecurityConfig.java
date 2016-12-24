cb214da166a1880360e59f3556623e5ff3c1deda
seven sins <ersanyu@163.com>
2016-12-24 17:17:58 +0800

    引入spring security, 未完成

---
+++ src/main/java/com/springboot/conf/WebSecurityConfig.java
@@ -0,0 +1,7 @@
+package com.springboot.conf;
+
+
+// @Configuration
+public class WebSecurityConfig  { // extends WebSecurityConfigurerAdapter
+
+}
--- src/main/java/com/springboot/domain/User.java
+++ src/main/java/com/springboot/domain/User.java
@@ -1,7 +1,14 @@
 package com.springboot.domain;
 
-public class User {
+import java.util.Collection;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class User implements UserDetails {
+
+	private static final long serialVersionUID = 1L;
+	
 	private String userId;
 	private String userName;
 	private String passWord;
@@ -22,6 +29,42 @@ public class User {
 	}
 	public void setPassWord(String passWord) {
 		this.passWord = passWord;
+	}
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return false;
 	}
 	
 }
